624711b8ed216a33e602a96758cc1acfb8679183
Marc Monnerat <procrastinatio@gmail.com>
2019-03-08 18:02:36 +0100

    support for pythn 3.6

--- setup.py
+++ setup.py
@@ -2,9 +2,22 @@ from setuptools import setup
 
 
 setup(name='esrijson',
-      version='0.4',
+      version='0.4.1',
       description='Bindings and utilities for EsriJSON',
-      classifiers=[],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Scientific/Engineering :: GIS',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
       license='BDS',
       author='Loic Gasser',
       author_email=u'loicgasser4@gmail.com',

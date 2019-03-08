98727bb2d346b9bd88247ba55228039102699ff4
Marc <procrastinatio@gmail.com>
2019-03-08 18:43:53 +0100

    Python 3.6 migration (#10)
    * support for pythn 3.6
    * add homepage
    * license and description
    * markdown should render well on pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,32 @@
 from setuptools import setup
 
 
+with open("README.md", "r") as fh:
+        long_description = fh.read()
+
 setup(name='esrijson',
-      version='0.4',
+      version='0.4.2',
       description='Bindings and utilities for EsriJSON',
-      classifiers=[],
-      license='BDS',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Scientific/Engineering :: GIS',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      license='BSD',
       author='Loic Gasser',
       author_email=u'loicgasser4@gmail.com',
+      url='https://github.com/geoadmin/lib-esrijson',
       packages=['esrijson'],
       package_dir={'esrijson': 'esrijson'},
       test_suite='nose.collector',
